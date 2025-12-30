c6f26f237458fa4449af5c263e63a2a5e5b091ef
th.kim <th.kim@hyperinfo.co.kr>
2025-12-30 18:25:35 +0900

    헤더 요소에 로고 슬롯 추가 및 템플릿 로딩 개선

--- public/assets/js/custom-elements.js
+++ public/assets/js/custom-elements.js
@@ -1,35 +1,57 @@
 import templateManager from "./template-manager.js";
 
-await templateManager.loaded;
-const templates = templateManager.templates;
-console.log('Templates available in HeaderElement:', templates);
+await templateManager.ready;
+// const templates = templateManager.templates;
+const headersTemplate = await templateManager.getTemplateById(
+  "header-template"
+);
+console.log("Templates available in HeaderElement:", headersTemplate);
 
 class HeaderElement extends HTMLElement {
+  ready = null;
+//   templateHTML = null;
+  tagHTML = null;
 
-    templateHTML = templates['header-template'].innerHTML;
+  constructor() {
+    super();
+    console.log("HeaderElement constructor called:", this.innerHTML);
+    this.ready = templateManager
+      .getTemplateById("header-template")
+      .then((templateHTML) => {
+        console.log("HeaderElement templateHTML loaded and set", templateHTML);
+        this.tagHTML = this.innerHTML;
+        this.innerHTML = templateHTML;
+        // this.innerHTML = templateHTML;
+      });
+  }
 
-    constructor() {
-        super();
-        console.log('HeaderElement constructor called');
-        // templateManager.getTemplateById('header-template').then(template => {
-        //     this.innerHTML = template?.innerHTML;
-        //     console.log('HeaderElement template loaded and set', template);
-        // });
-        //
-        this.innerHTML = `<h1>Header Component Loaded</h1>`;
-    }
-    
-    connectedCallback() {
-        this.innerHTML = this.templateHTML;
-        const $p = this.querySelector('h1');
-        if ($p) {
-            $p.style.cursor = 'pointer';
-            $p.onclick = () => {
-                console.log('HeaderElement paragraph clicked');
-            }
-        }
-        console.log('HeaderElement connected to the DOM');
-    }
+  async connectedCallback() {
+    await this.ready.then(() => {
+      console.log("HeaderElement ready resolved in connectedCallback");
+    });
+    // this.innerHTML = this.templateHTML;
+    console.log("HeaderElement content set from templateHTML");
+  }
+  //   const element = document.createElement("div");
+  //   element.innerHTML = this.tagHTML;
+  //   const slots = element.querySelectorAll("slot");
+  //   slots.forEach((slot) => {
+  //     const name = slot.getAttribute("name");
+  //     if (name) {
+  //       const assignedElements = this.querySelectorAll(`[slot="${name}"]`);
+  //       assignedElements.forEach((el) => {
+  //         slot.replaceWith(el);
+  //       });
+  //     }
+  //   });
+  //   const $p = this.querySelector("h1");
+  //   if ($p) {
+  //     $p.style.cursor = "pointer";
+  //     $p.onclick = () => {
+  //       console.log("HeaderElement paragraph clicked");
+  //     };
+  //   }
+  //   console.log("HeaderElement connected to the DOM");
 }
 
 // customElements.define('header-element', HeaderElement);
--- public/assets/js/template-manager.js
+++ public/assets/js/template-manager.js
@@ -1,45 +1,110 @@
+// console.log("caches" in window);
+// console.log(window.caches);
+const cacheName = "newCache";
+const templateUrl = "/templates.html";
+// caches.open(cacheName).then((cache) => {
+//   cache.addAll(templateUrls).then(() => {
+//     console.log("Cache add Success");
+//   });
+// });
+
 class TemplateManager {
-    
-    static instance = null;
-    
-    templates = {};
-   
-    constructor() {
-        console.log('caches' in window)
-        console.log(window.caches)
-        console.log('TemplateManager constructor called');
-        if (TemplateManager.instance) {
-            return TemplateManager.instance;
-        }
-        this.loaded = this.loadTemplates();
+  static instance = null;
+  ready = null;
+  templateCache = null;
+  // templates = null;
+  
+  constructor() {
+    console.log("TemplateManager constructor called");
+    if (TemplateManager.instance) {
+      return TemplateManager.instance;
     }
 
-    loadTemplates() {
-        console.log('Loading templates...');
-        return fetch('/templates.html')
-            .then(response => response.text())
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                const templateElements = doc.querySelectorAll('template');
-                templateElements.forEach(template => {
-                    this.templates[template.id] = template.cloneNode(true);
-                });
-                console.log('Templates parsed:', this.templates);
-                TemplateManager.instance = this;
-                return true;
-            })
-            .catch(error => {
-                console.error('Error loading templates:', error);
-            });
+    // this.ready = caches.open(cacheName).then((cache) => {
+    //   cache.addAll(templateUrl)
+    //   this.templateCache = cache;
+    // });
+
+    this.ready = this.init(cacheName, templateUrl);
+  }
+
+  async init(cacheName, templateUrl) {
+    const cache = await caches.open(cacheName);
+    console.log("Opened cache:", cacheName);
+    await cache.add(templateUrl);
+    console.log("Cache add Success:", templateUrl);
+    this.templateCache = cache;
+    // const response = await cache.match(templateUrl);
+    // if (response) {
+    //   const html = await response.text();
+    //   const parser = new DOMParser();
+    //   const doc = parser.parseFromString(html, "text/html");
+    //   const templateElements = doc.querySelectorAll("template");
+    //   this.templates = this.templates || {};
+    //   templateElements.forEach((template) => {
+    //     this.templates[template.id] = template.cloneNode(true);
+    //   });
+    //   console.log("Templates parsed:", this.templates);
+    //   TemplateManager.instance = this;
+    // } else {
+    //   console.warn("No cached templates.html found.");
+    // }
+  }
+
+  // async openCache(cacheName, templateUrls) {
+  //   const cache = await caches.open(cacheName);
+  //   await cache.add(templateUrls[0]);
+  //   this.templateCache = cache;
+  //   console.log("Cache addAll Success");
+  // }
+
+  // async loadTemplates() {
+    
+  //   await this.openCache(cacheName, templateUrl);
+    
+  //   console.log("Loading templates...");
+    
+  //   try {
+  //     const response = await fetch("/templates.html");
+  //     const html = await response.text();
+  //     const parser = new DOMParser();
+  //     const doc = parser.parseFromString(html, "text/html");
+  //     const templateElements = doc.querySelectorAll("template");
+  //     this.templates = this.templates || {};
+  //     templateElements.forEach((template) => {
+  //       this.templates[template.id] = template.cloneNode(true);
+  //     });
+  //     console.log("Templates parsed:", this.templates);
+  //     TemplateManager.instance = this;
+  //   } catch (error) {
+  //     console.error("Error loading templates:", error);
+  //   }
+  // }
+
+  async getTemplateById(id) {
+    await this.ready.then(() => {
+      console.log(`Template requested: ${id}`);
+    });
+    
+    let response = await this.templateCache.match(templateUrl);
+
+    if(!response) {
+      response = await fetch(templateUrl);
+      console.log('Fetched templates.html from network:', templateUrl);
     }
 
-    async getTemplateById(id) {
-        await this.loaded.then(() => {
-            console.log(`Template requested: ${id}`);
-            return (this.templates[id]) ? this.templates[id].innerHTML : null;
-        });
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const template = doc.getElementById(id);
+    if (template) {
+      console.log(`Template ${id} found.`);
+      return template.innerHTML;
+    } else {
+      console.warn(`Template ${id} not found.`);
+      return null;
     }
+  }
 }
 
 export default new TemplateManager();
